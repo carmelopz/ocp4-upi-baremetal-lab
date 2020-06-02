93619fe4f4d02571641c0b55abada2a3f7be4f32
Samuel Veloso <veloso.lopez.samuel@gmail.com>
2020-06-02 13:38:10 +0200

    Install oc as requirement

--- requirements.sh
+++ requirements.sh
@@ -8,6 +8,7 @@ set -o pipefail # return the exit code of the last command that threw a non-zero
 TF_VERSION="0.12.26"
 TF_PROVIDERS_DIR="${HOME}/.terraform.d/plugins"
 TF_LIBVIRT_PROVIDER_VERSION="v0.6.2/terraform-provider-libvirt-0.6.2+git.1585292411.8cbe9ad0.Fedora_28.x86_64.tar.gz"
+OC_VERSION="4.3"
 FCCT_VERSION="0.5.0"
 
 # install_terraform <installation_dir> <terraform_version>
